1730f72cb3cdb0f696f1bda01afbfd3e480b6c5f
dazeb <daz@dazeb.dev>
2024-07-16 22:13:51 +0100

    Upgraded to next 14

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
+  // Remove the experimental.appDir option as it's no longer needed
 }
 
 export default nextConfig
